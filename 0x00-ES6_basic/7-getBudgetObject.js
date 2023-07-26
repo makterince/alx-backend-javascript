99b07d66714801a6ea01a4dfdf8aed96671243f0
Festus Omaku <fomaku3@gmail.com>
2023-07-26 12:07:58 +0100

    rest param

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,11 @@
+export default function getBudgetObject(income, gdp, capita) {
+  const income;
+  const gdp;
+  const capita;
+  const budget = {
+    income,
+    gdp,
+    capita
+  };
+  return budget;
+}
